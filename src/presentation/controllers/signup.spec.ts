396be99d80d43d5c1d32c40dc32c368feb2618d2
Raphael Neves <raphaelneves@MacBook-Pro-de-Raphael.local>
2021-07-21 21:25:35 -0300

    feat: ensure SignUpController return email error

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,7 @@
 import { SignUpControllers } from './signup'
 
 describe('SignUp Controller', () => {
-  test('Should return 400 if no name is provided ', () => {
+  test('Should return 400 if no name is provided', () => {
     const sut = new SignUpControllers()
     const httpRequest = {
       body: {
@@ -13,5 +13,19 @@ describe('SignUp Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpControllers()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 })
--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,17 @@
 export class SignUpControllers {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
     }
   }
 }
