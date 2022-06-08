06f6f04801a3f61f3c1bbe52c8ba7066dc5e29be
Raphaenn <raphaelnn@hotmail.com>
2022-06-08 12:33:41 -0300

    refactor: remove invalid test

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -32,33 +32,6 @@ const makeSut = (): SutTypes => {
 }
 
 describe('Decorator unit test', () => {
-  test('should return handle when LogControllerDecorator is called', async () => {
-    class ControllerStub implements Controller {
-      async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            name: 'any_name',
-            email: 'invalid_email@mail.com'
-          }
-        }
-        return await new Promise(resolve => resolve(httpResponse))
-      }
-    }
-    const controllerStub = new ControllerStub()
-    const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const sut = new LogControllerDecorator(controllerStub)
-    const httpRequest = {
-      body: {
-        name: 'any_name',
-        email: 'invalid_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
-    await sut.handle(httpRequest)
-    expect(handleSpy).toBeCalledWith(httpRequest)
-  })
   test('should return LogControllerDecorator with the same controller return', async () => {
     const { sut, controllerStub } = makeSut()
     const httpRequest = {
