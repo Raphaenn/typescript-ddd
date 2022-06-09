c47363c5f34a0bd2b234bb910dae1ec558a0b7e8
Raphaenn <raphaelnn@hotmail.com>
2022-06-09 15:24:41 -0300

    feat: add LogMongoRepository to signFactory

--- src/infra/db/mongodb/account-repository/AccountMongoRepository.ts
+++ src/infra/db/mongodb/account-repository/AccountMongoRepository.ts
@@ -5,7 +5,7 @@ import { MongoHelper } from '../helper/mongo-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     const { insertedId } = await accountCollection.insertOne(accountData)
     const collection = await accountCollection.findOne(insertedId)
     return MongoHelper.map(collection)
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -5,6 +5,7 @@ import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/AccountMongoRepository'
 import { Controller } from 'presentation/protocols'
 import { LogControllerDecorator } from '../decorators'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 
 export const makeSingupController = (): Controller => {
   const salt = 12
@@ -13,5 +14,6 @@ export const makeSingupController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const signUpControllers = new SignUpControllers(emailValidatorAdapter, dbAddAccount)
-  return new LogControllerDecorator(signUpControllers)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpControllers, logMongoRepository)
 }
