9c731e7584b46b88dd263eba8d88ee4eb01eeb16
Raphaenn <raphaelnn@hotmail.com>
2022-06-18 01:30:17 -0300

    test: should not return if validation succeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,10 +1,20 @@
 import { MissingParamError } from '../../errors/missing-param-error'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation('field')
+}
+
 describe('Required fields validation', () => {
   test('should return MissingParamError if validation fails', () => {
-    const sut = new RequiredFieldValidation('field')
-    const validated = sut.validate({ name: 'wrong_field' })
-    expect(validated).toEqual(new MissingParamError('field'))
+    const sut = makeSut()
+    const error = sut.validate({ name: 'wrong_field' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+
+  test('should not return if validation succeds', () => {
+    const sut = makeSut()
+    const error = sut.validate({ field: 'any_name' })
+    expect(error).toBeFalsy()
   })
 })
