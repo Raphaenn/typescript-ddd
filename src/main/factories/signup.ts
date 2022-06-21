f0b15490d97819de68069e1a39fe0e4850dbd867
Raphaenn <raphaelnn@hotmail.com>
2022-06-20 22:56:43 -0300

    feat: ensure login controller returns a correct value when validate

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,5 +1,4 @@
 import { SignUpControllers } from '../../presentation/controllers/signup/signup'
-import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/AccountMongoRepository'
@@ -10,13 +9,12 @@ import { makeSignupValidation } from './signup-validation'
 
 export const makeSingupController = (): Controller => {
   const salt = 12
-  const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const validations = makeSignupValidation()
 
-  const signUpControllers = new SignUpControllers(emailValidatorAdapter, dbAddAccount, validations)
+  const signUpControllers = new SignUpControllers(dbAddAccount, validations)
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(signUpControllers, logMongoRepository)
 }
