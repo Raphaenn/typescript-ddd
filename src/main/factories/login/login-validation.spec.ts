b17d83e40f8f883cfefcc2956a5573f17b15d186
Raphaenn <raphaelnn@hotmail.com>
2022-06-22 22:29:53 -0300

    feat: should call login validatoin composite with all validations

---
+++ src/main/factories/login/login-validation.spec.ts
@@ -0,0 +1,31 @@
+import { EmailValidator } from '../../../presentation/protocols/emailValidator'
+import { Validation, RequiredFieldValidation, ValidationComposite, EmailValidation } from '../../../presentation/helpers/validators'
+import { makeLoginValidation } from './login-validation'
+
+jest.mock('../../../presentation/helpers/validators/validation-composite')
+
+const makeEmailValidate = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+describe('Login Validation factory', () => {
+  test('should call validatoin composite with all validations', () => {
+    makeLoginValidation()
+
+    const validations: Validation[] = []
+    const fields = ['email', 'password']
+
+    for (const field of fields) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+
+    validations.push(new EmailValidation('email', makeEmailValidate()))
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
---
+++ src/main/factories/login/login-validation.ts
@@ -0,0 +1,15 @@
+import { Validation } from 'presentation/helpers/validators'
+import { ValidationComposite, RequiredFieldValidation, EmailValidation } from '../../../presentation/helpers/validators'
+import { EmailValidatorAdapter } from '../../../utils/email-validator-adapter'
+
+export const makeLoginValidation = (): ValidationComposite => {
+  const requiredFields: Validation[] = []
+  const emailValidator = new EmailValidatorAdapter()
+
+  for (const fields of ['email', 'password']) {
+    requiredFields.push(new RequiredFieldValidation(fields))
+  }
+  requiredFields.push(new EmailValidation('email', emailValidator))
+
+  return new ValidationComposite(requiredFields)
+}
