cc74d96ad74b5f25029fd7720ad8f7c52b386b37
Raphaenn <raphaelnn@hotmail.com>
2022-07-08 22:00:54 -0300

    refactor: rename signup routes with login routes

---
+++ src/main/routes/login-router.test.ts
@@ -0,0 +1,46 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helper/mongo-helper'
+
+describe('Login Router', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  describe('post/ signup', () => {
+    test('should return an account on success', async () => {
+      await request(app)
+        .post('/api/signUp')
+        .send({
+          name: 'Raphael Neves',
+          email: 'raphaelnn@hotmail.com',
+          password: '321993',
+          passwordConfirmation: '321993'
+        })
+        .expect(200)
+    })
+  })
+
+  describe('post/ signIn', () => {
+    test('should return an account on success', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Raphael Neves',
+          email: 'raphaelnn@hotmail.com',
+          password: '321993',
+          passwordConfirmation: '321993'
+        })
+        .expect(200)
+    })
+  })
+})
